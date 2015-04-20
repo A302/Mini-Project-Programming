e7c9d12761715d532b48df0cb5874768a57e7dbf
crusantos <Simonfjordvald@gmail.com>
2015-04-20 10:47:45 +0200

    How to play has been given contents now with a headline and text.
    Strings are added in strings.xml file

--- app/src/main/java/com/yourdomain/physicalsnakegame/HowtoPlayActivity.java
+++ app/src/main/java/com/yourdomain/physicalsnakegame/HowtoPlayActivity.java
@@ -14,7 +14,6 @@ public class HowtoPlayActivity extends ActionBarActivity {
         setContentView(R.layout.activity_howto_play);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
