a591d9034f0410d0f88f325587c0833f986fab79
crusantos <Simonfjordvald@gmail.com>
2015-04-21 10:33:20 +0200

    Added four markers, some comments and deleted a few things.
    Added a polygon instead of four lines that connect to eachother

--- app/src/main/java/com/yourdomain/physicalsnakegame/GameActivity.java
+++ app/src/main/java/com/yourdomain/physicalsnakegame/GameActivity.java
@@ -1,60 +1,87 @@
 package com.yourdomain.physicalsnakegame;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 
 public class GameActivity extends MenuActivity
-        implements OnMapReadyCallback{
 
-
+        implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
- /*
-        The code below implements the map fragment from the related xml file and makes google map appear on the screen.*/
+        /*The code below implements the map fragment from the related xml file and makes google map appear on the screen.*/
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-/*
-    The method "onMapReady" initiates when google map has succesfully loaded on to the screen.
-    */
+
+    /*The method "onMapReady" initiates when google map has succesfully loaded on to the screen.*/
     @Override
     public void onMapReady(GoogleMap map) {
-        map.setMyLocationEnabled(true);/* Connects the devices GPS */
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        /*Tracks the phone's current location*/
+        /* Connects the devices GPS */
+        map.setMyLocationEnabled(true);
+
+        /*Sets the camera to a specific location*/
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                new LatLng(57.055993, 9.903725), 20));
-        // Polylines are useful for marking paths and routes on the map.
-        map.addPolyline(new PolylineOptions().geodesic(true)
-                        .add(new LatLng(57.055800, 9.903500))
-                        .add(new LatLng(57.055800, 9.903950))
-                        .add(new LatLng(57.056186, 9.903950))
-                        .add(new LatLng(57.056186, 9.903500))
-                        .add(new LatLng(57.055800, 9.903500))
-        );
+                new LatLng(57.055993, 9.903725), 19.7f));
 
+         /* Disables the option to drag the map across the screen, as well as zoom, tilt and rotation. The user basically can't do any actions
+        setAllGesturesEnabled is set to false */
+        mapFragment.getMap().getUiSettings().setAllGesturesEnabled(false);
 
+        /* Polylines are useful for marking paths and routes on the map.
+        added a polygon instead of a polyline - it is one cohesive figure appearing as a rectangle
+         */
+        map.addPolygon(new PolygonOptions()
+        .add(new LatLng(57.055800, 9.903500))
+        .add(new LatLng(57.055800, 9.903950))
+        .add(new LatLng(57.056186, 9.903950))
+        .add(new LatLng(57.056186, 9.903500)));
+                map.addMarker(new MarkerOptions()
+                        .title("Point 1")
+                        .alpha(1f)
+                        .position(new LatLng(57.056022, 9.903823)));
+                map.addMarker(new MarkerOptions()
+                         .alpha(0.01f)
+                         .title("Point 2")
+                         .position(new LatLng(57.056100, 9.903750)));
+                map.addMarker(new MarkerOptions()
+                         .title("Point 3")
+                         .alpha(0.01f)
+                         .position(new LatLng(57.055996, 9.903872)));
+                map.addMarker(new MarkerOptions()
+                         .title("Point 4")
+                         .alpha(0.01f)
+                         .position(new LatLng(57.055984, 9.903743)));
     }
-/* Creates a marker taking position from the point "sydney" */
-        /*map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
-    }*/
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
